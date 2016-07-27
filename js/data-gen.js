a4dba1c601b1e8884e6a972919fa3ed1383f64f6
Onna Nelson <onna.nelson@gmail.com>
2016-07-27 15:36:28 -0700

    Form updates
    Working on getting DOM to pull data from form.

--- js/data-gen.js
+++ js/data-gen.js
@@ -1,8 +1,27 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 var button = document.querySelector('button');
+var rowNum = document.querySelector('input.numRow').value;
+var colNum = document.querySelector('input.numCol').value;
+var text = document.querySelector('input.othertext').value;
 
 button.addEventListener('click', function(ev){
-   var text = document.body.querySelector('input').value;
-   
+	console.log(colNum);
+	console.log(rowNum);
+	console.log("is this working?");
+	console.log(text);
+	var field = document.querySelector('#field')
+   field.innerHTML = text;
 })
+
+})
+
+
+
+// save table to CSV links: 
+// https://jsfiddle.net/terryyounghk/kpegu/
+// https://www.raymondcamden.com/2014/04/03/Dynamically-Creating-CSV-Files-on-the-Client/
+// http://halistechnology.com/2015/05/28/use-javascript-to-export-your-data-as-csv/
+// http://blog.eliacontini.info/post/79860720828/export-to-csv-using-javascript-and-the-download
+// https://adilapapaya.wordpress.com/2013/11/15/exporting-data-from-a-web-browser-to-a-csv-file-using-javascript/
+
